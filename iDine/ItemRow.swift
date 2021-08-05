1a12740320f325afab07f2c455b45459a00b9638
Yi An Chen <ianchen0419@gmail.com>
2021-08-06 00:13:17 +0800

    item row

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,29 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Yi An Chen on 2021/8/5.
+//  Copyright © 2021 cheyian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
