99aacea984291cc129c5f9007a609b91378169cd
Yi An Chen <ianchen0419@gmail.com>
2021-08-07 10:31:37 +0800

    order view

---
+++ iDine/OrderView.swift
@@ -0,0 +1,44 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Yi An Chen on 2021/8/7.
+//  Copyright © 2021 cheyian. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Check out")) {
+                        Text("Please Order")
+                    }
+                }
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
